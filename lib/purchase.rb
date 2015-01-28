2be815b7a14b2a31c9dee5d3836727a7e6db3d47
Christopher Joy-Webb & Sean Holstrom <student@epicodus.com>
2015-01-27 16:52:06 -0800

    Failed attempt at scope

--- lib/purchase.rb
+++ lib/purchase.rb
@@ -1,3 +1,17 @@
 class Purchase < ActiveRecord::Base
   has_and_belongs_to_many :products
+
+  scope(:between, lambda do |start_date, end_date|
+    where("date >= :start_date AND date <= :end_date",
+      { :start_date => start_date, :end_date => end_date })
+  end)
+
+
+  define_method(:total) do
+    total = 0
+    self.products.each do |product|
+      total += product.price # total = product.price.+(total) longhand
+    end
+    total
+  end
 end
